0678278e22ae35df7b83488602d1ca309bba0220
Erhan Koral <erhan.16k@gmail.com>
2024-01-30 14:07:41 +0100

    assignment w1

---
+++ hackyourtemperature/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('hello from backend to frontend!');
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
+
+app.post('/weather', (req, res) => {
+  const cityName = req.body.cityName;
+  res.send(`You submitted: ${cityName}`);
+});
