dbd53757250057ea4f39f60c747bcefe5db492b9
Erhan Koral <erhan.16k@gmail.com>
2024-02-01 13:16:23 +0100

    requested changes-1

--- hackyourtemperature/server.js
+++ hackyourtemperature/server.js
@@ -1,17 +1,24 @@
 import express from 'express';
 
 const app = express();
+const PORT = 3000;
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('hello from backend to frontend!');
 });
 
-app.listen(3000, () => {
+app.post('/weather', (req, res) => {
+  const {cityName} = req.body;
+  if(!cityName){
+    res.send('City Name is required!');
+  } else {
+    res.send(`You submitted: ${cityName}`);
+  }
+});
+
+app.listen(PORT, () => {
   console.log('Server is listening on port 3000');
 });
 
-app.post('/weather', (req, res) => {
-  const cityName = req.body.cityName;
-  res.send(`You submitted: ${cityName}`);
-});
