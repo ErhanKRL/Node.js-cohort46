6864fed83896862d2c23d57ec9ea37b48708e2b2
Erhan Koral <erhan.16k@gmail.com>
2024-02-02 22:09:47 +0100

    review-2 requested changes done

--- hackyourtemperature/server.js
+++ hackyourtemperature/server.js
@@ -12,10 +12,9 @@ app.get('/', (req, res) => {
 app.post('/weather', (req, res) => {
   const {cityName} = req.body;
   if(!cityName){
-    res.send('City Name is required!');
-  } else {
-    res.send(`You submitted: ${cityName}`);
+    return res.status(400).json({ error: "CityName is required in the request body." });
   }
+  res.send(`You submitted: ${cityName}`);
 });
 
 app.listen(PORT, () => {
