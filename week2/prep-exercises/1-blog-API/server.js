18fcf8d205b539e4f2a58f4acc4ccf869544490a
Erhan Koral <erhan.16k@gmail.com>
2024-02-09 15:26:25 +0100

    changes according to lesson

--- week2/prep-exercises/1-blog-API/server.js
+++ week2/prep-exercises/1-blog-API/server.js
@@ -1,81 +1,100 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
 const app = express();
 const fs = require('fs');
 
 app.use(express.json());
 
-app.post('/blogs', (req, res) => {
+app.post('/posts', (req, res) => {
   const { title, content } = req.body;
     if (!title || !content) {
         res.status(400).send('Title and content are required.');
         return;
     }
-  fs.writeFileSync(title.split(' ').join('-'), content);
-  res.end('ok')
+    try {
+      fs.writeFileSync(path.join(__dirname, `${title}.json`), JSON.stringify({title, content}));
+    } catch (error) {
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+  res.status(201).send('Blog post created successfully');
+})
+
+app.get('/posts/:title', (req, res) => {
+  const {title} = req.params;
+  if (!title) {
+    res.status(400).send('Title is required.');
+    return;
+  }
+  const isExist = fs.existsSync((path.join(__dirname, `${title}.json`)));
+  if(isExist) {
+    try {
+    const postContent = fs.readFileSync(path.join(__dirname, `${title}.json`));
+    res.status(200).send(postContent);
+  } catch (error){
+    res.status(500).send('Internal Server Error');
+    return;
+  }
+  } else {
+    res.status(404).send('Not Found');
+  }
+  
 })
 
 app.put('/posts/:title', (req, res) => {
   const {title} = req.params;
   const {content} = req.body;
-
   if (!content) {
     res.status(400).send('Content is required.');
     return;
 }
-  if (fs.existsSync(title)) {
-    fs.writeFileSync(title, content);
-    res.end('ok')
-  }
-  else {
-    res.status(404).send('This post does not exist!');
-  }
-})
-
-app.delete('/blogs/:title', (req, res) => {
-  const {title} = req.params;
-
-  if (fs.existsSync(title)) {
-    fs.unlinkSync(title);
-    res.end('ok')
-  }
-  else {
-    res.status(404).send('This post does not exist!');
-  }
-})
-
-app.get('/blogs/:title', (req, res) => {
-  const {title} = req.params;
-
-  if (fs.existsSync(title)) {
-    const post = fs.readFileSync(title);
-    res.end(post)
-  }
-  else {
-    res.status(404).send('This post does not exist!');
-  }
-})
-
-app.get('/blogs', (req, res) => {
-  const selectedBlogPosts = []
-  const titles = req.body
-  titles.forEach((title) => {
-    let blogPost = {}
-    const blogTitle = title.title.split(' ').join('-')
-    if (!fs.existsSync(blogTitle)){
-      blogPost = {
-        title: title.title,
-        content: "No blog post with this title"
-      }
-    } else {
-    const post = fs.readFileSync(blogTitle, 'utf8');
-    blogPost = {
-      title: title.title,
-      content: post
+  const isExist = fs.existsSync(`${title}.json`);
+  if (isExist) {
+    try {
+      fs.writeFileSync(`${title}.json`, JSON.stringify({title, content}));
+    } catch (error) {
+      res.status(500).send('Internal Server Error');
+      return;
     }
+    res.status(200).send('Content is updated successfully');
   }
-    selectedBlogPosts.push(blogPost);
-  })
-  res.send(selectedBlogPosts);
+  else {
+    res.status(404).send('This post does not exist!');
+  }
+})
+
+app.delete('/posts/:title', (req, res) => {
+  const {title} = req.params;
+  const isExist = fs.existsSync(path.join(__dirname, `${title}.json`));
+  if (isExist) {
+    fs.unlinkSync(path.join(__dirname, `${title}.json`));
+    res.end('ok')
+  }
+  else {
+    res.status(404).send('This post does not exist!');
+  }
+})
+
+
+app.get('/posts', (req, res) => {
+  try {
+    const files = fs.readdirSync(__dirname);
+    const postFiles = files.filter((file) => path.extname(file) === ".json");
+    const fileContents = [];
+
+    postFiles.forEach((file) => {
+      try {
+        const postFilePath = path.join(__dirname, file);
+        const data = fs.readFileSync(postFilePath, "utf-8");
+        fileContents.push({ fileName: file, content: data });
+      } catch (error) {
+        res.status(500).send(`Error reading file: ${error}`);
+      }
+    });
+    res.status(200).send(fileContents);
+  } catch (error) {
+    res.status(500).send(`Error reading directory: ${error}`);
+  }
 })
  
 
