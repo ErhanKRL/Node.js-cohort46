26d49575464470d645c72696a52e745467f0b0e5
Erhan Koral <erhan.16k@gmail.com>
2024-02-10 12:49:07 +0100

    practice-1 completed

--- week3/practice-exercises/1-basic-authentication/script.js
+++ week3/practice-exercises/1-basic-authentication/script.js
@@ -8,8 +8,17 @@
  * Hints:
  * - for basic authentication the username and password need to be base64 encoded
  */
-function printBooks() {
-  // YOUR CODE GOES IN HERE
+async function printBooks() {
+  try{
+    const response = await fetch('https://restapiabasicauthe-sandbox.mxapps.io/api/books', {
+    headers: { 'Authorization': 'Basic YWRtaW46aHZnWDhLbFZFYQ==' }
+  });
+  const data = await response.json();
+  console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+  
 }
 
 printBooks();
